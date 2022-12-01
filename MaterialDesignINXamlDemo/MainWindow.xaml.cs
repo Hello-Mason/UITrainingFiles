f95e4552be2e0917108b9df674ed7b7c01050b78
Mason Tian <chinesebimer@gmail.com>
2022-12-01 10:16:04 +0800

    根据鼠标颜色触发不同样式风格ListItemContainer

--- MaterialDesignINXamlDemo/MainWindow.xaml.cs
+++ MaterialDesignINXamlDemo/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MaterialDesignINXamlDemo
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
